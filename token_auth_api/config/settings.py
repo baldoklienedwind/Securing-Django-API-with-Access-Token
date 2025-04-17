757d339bfabf68a1ed5e3f995125aa84bdef2452
baldoklienedwind <baldoklienedwind@gmail.com>
2025-04-17 12:04:41 +0400

    whhhhhhhh

--- token_auth_api/config/settings.py
+++ token_auth_api/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'api',
+    'corsheaders',
     
 ]
 
@@ -61,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
