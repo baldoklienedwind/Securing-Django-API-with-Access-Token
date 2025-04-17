82c2e241dfb711db865f7c81bb779f543b126b95
baldoklienedwind <baldoklienedwind@gmail.com>
2025-04-17 12:39:45 +0400

    Update views.py

--- token_auth_api/api/views.py
+++ token_auth_api/api/views.py
@@ -2,11 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+import logging
+
+logger = logging.getLogger(__name__)
 
 class SecureHelloView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        logger.info(f"User: {request.user.username}")
         return Response({"message": f"Hello, {request.user.username}!"})
-
