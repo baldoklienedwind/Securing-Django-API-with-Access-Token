3e10ff8252c85d5e8a69835fa8af04d34ace8bef
baldoklienedwind <baldoklienedwind@gmail.com>
2025-04-19 10:40:28 +0400

    ahhhhhhhhhhhhh

--- token_auth_api/api/views.py
+++ token_auth_api/api/views.py
@@ -1,13 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 import logging
 
 logger = logging.getLogger(__name__)
 
 class SecureHelloView(APIView):
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
--- token_auth_api/config/settings.py
+++ token_auth_api/config/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'rest_framework.authtoken',
-    'api'
+    'api',
     'corsheaders',
 ]
 
